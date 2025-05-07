4ac90a01b3f3e5aa926dfbd219a861888bb2fabd
John Doe <johndoe@email.com>
2025-05-07 14:47:03 +0300

    build.sh-Added

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Render.com için build script
+
+# Hata durumunda script'i durdur
+set -e
+
+# .NET SDK'yı kur
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0
+export PATH="$HOME/.dotnet:$PATH"
+export DOTNET_ROOT="$HOME/.dotnet"
+
+# Projeyi derle
+dotnet publish -c Release -o out
