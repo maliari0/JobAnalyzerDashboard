54cce3da998b88a4e459dfe9e47996ea22c26eeb
John Doe <johndoe@email.com>
2025-05-07 14:57:09 +0300

    start.sh-Created

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Render.com için start script
+
+# Hata durumunda script'i durdur
+set -e
+
+# .NET SDK'yı kur (runtime için)
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0
+export PATH="$HOME/.dotnet:$PATH"
+export DOTNET_ROOT="$HOME/.dotnet"
+
+# Uygulamayı başlat
+cd out && dotnet JobAnalyzerDashboard.Server.dll
