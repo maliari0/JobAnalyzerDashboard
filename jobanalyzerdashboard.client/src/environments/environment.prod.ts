72fad7c0139107c11e2021c0f219a54177259e05
John Doe <johndoe@email.com>
2025-05-08 11:26:36 +0300

    environment.prod-Changes

--- jobanalyzerdashboard.client/src/environments/environment.prod.ts
+++ jobanalyzerdashboard.client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://jobanalyzerdashboard-api.onrender.com',
+  apiUrl: 'https://jobanalyzerdashboard.onrender.com',
   n8nWebhookUrl: 'https://n8n-service-a2yz.onrender.com/webhook/apply-auto'
 };
